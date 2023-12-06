1f3f3e4dfadbd1dcdd53015682f8aade103d2f6e
Gerard Mortel <gmortel@us.ibm.com>
2023-12-06 13:00:11 -0600

    Initial commit

--- 01_driver.sh
+++ 01_driver.sh
@@ -2,4 +2,11 @@
 
 echo "#### Running the driver file"
 . ./02_setup_env.sh
-./03_create_namespace.sh
+./03_create_elastic_search_namespace.sh
+./04_create_logging_namespace.sh
+./05_install_elastic_search_operator.sh
+./06_create_elastic_search_subscription.sh
+./07_install_logging_operator.sh
+./08_create_logging_subscription.sh
+./09_create_cluster_logging_resource.sh
+./10_create_service_account.sh
