20e9ebaaf5e2b3ac9ee59015d2a8880fd3d85f84
Gerard Mortel <gmortel@us.ibm.com>
2023-12-06 12:28:30 -0600

    Added storage class

--- 02_setup_env.sh
+++ 02_setup_env.sh
@@ -3,6 +3,7 @@
 echo "#### Running set up the environment variables script"
 # Higher priority variables
 export NS="custom-log-forwarding"
+export STORAGECLASS="nfs-managed-storage" # Fyre NFS Storage
 
 # Lower priority variables
 
