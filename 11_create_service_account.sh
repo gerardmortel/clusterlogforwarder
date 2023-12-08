a1e058f46a7a7a923f1a83bb92acc95d35c1252b
Gerard Mortel <gmortel@us.ibm.com>
2023-12-08 11:51:51 -0600

    Clarified comments

--- 11_create_service_account.sh
+++ 11_create_service_account.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # From https://docs.openshift.com/container-platform/4.12/logging/log_collection_forwarding/log-forwarding.html
-
+https://docs.openshift.com/container-platform/4.12/logging/log_collection_forwarding/log-forwarding.html
 echo "#### Extra 1. Create a service account for the collector. If you want to write logs to storage that requires a token for"
 echo "#### authentication, you must include a token in the service account."
 oc create serviceaccount ${SERVICEACCOUNTNAME} -n ${NS}
 
-echo "#### Add collect-application-logs role to ${SERVICEACCOUNTNAME}"
+echo "#### 2. Bind the appropriate cluster roles to the service account:"
 oc adm policy add-cluster-role-to-user \
 collect-application-logs system:serviceaccount:${NS}:${SERVICEACCOUNTNAME}
